be90a9056c51b4ed68a6dabf935e7d618f708702
mjainamshah <jainam.shah@student.moringaschool.com>
2024-01-08 11:26:17 +0300

    COMPLETED

--- server/app.py
+++ server/app.py
@@ -3,3 +3,22 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+# append to server/app.py
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+
+# append to server/app.py
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+
+# append to server/app.py
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
